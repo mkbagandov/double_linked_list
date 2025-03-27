6a08944c4664c3a95d0ae64cef9c051e82326398
mkbagandov <mkbagandov@gmail.com>
2025-03-27 10:58:26 +0300

    v2

--- double_linked_list.go
+++ double_linked_list.go
@@ -1,58 +1,67 @@
 package double_linked_list
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type Node struct {
-	val  string
-	next *Node
-	prev *Node
+type node struct {
+	val  map[string]string
+	next *node
+	prev *node
 }
 
 type DoubleLinkedList struct {
-	tail *Node
-	head *Node
+	tail *node
+	head *node
 }
 
-func (l *DoubleLinkedList) Append(val string) {
+func (l *DoubleLinkedList) Append(val map[string]string) {
 	if l.head == nil {
-		l.head = &Node{val: val}
+		l.head = &node{val: val}
 		l.tail = l.head
 		return
 	}
-	l.tail.next = &Node{val: val, prev: l.tail}
+	l.tail.next = &node{val: val, prev: l.tail}
 	l.tail = l.tail.next
 }
 
-func (l *DoubleLinkedList) Prepend(val string) {
+func (l *DoubleLinkedList) Prepend(val map[string]string) {
 	if l.head == nil {
-		l.head = &Node{val: val}
+		l.head = &node{val: val}
 		l.tail = l.head
 		return
 	}
-	l.head.prev = &Node{val: val, next: l.head}
+	l.head.prev = &node{val: val, next: l.head}
 	l.head = l.head.prev
 }
 
-func (l *DoubleLinkedList) Print() {
+func (l *DoubleLinkedList) Print() *[]map[string]string {
 	if l.head != nil {
+		result := make([]map[string]string, 1)
 		for {
-			fmt.Println(l.head.val)
+			result = append(result, l.head.val)
 			if l.head.next == nil {
 				break
 			}
 			l.head = l.head.next
 		}
+		return &result
 	}
+	return nil
 }
 
-func (l *DoubleLinkedList) PrintReverse() {
+func (l *DoubleLinkedList) PrintReverse() *[]map[string]string {
 	if l.tail != nil {
+		result := make([]map[string]string, 1)
 		for {
 			fmt.Println(l.tail.val)
+			result = append(result, l.tail.val)
 			if l.tail.prev == nil {
 				break
 			}
 			l.tail = l.tail.prev
 		}
+		return &result
 	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mkbagandov/double_linked_list
+module github.com/mkbagandov/double_linked_list/v2
 
 go 1.23.2
